abe6c9329d1d661a6a7fceb2360472af6992151a
Balqeesalawi <balqeesalawi195@gmail.com>
2025-11-10 22:50:01 +0300

    add Auction model

--- ANTICA/main/models.py
+++ ANTICA/main/models.py
@@ -1,15 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
+
+CATEGORY = (
+    ('art', 'Art'),
+    ('car', 'Cars'),
+    ('jewelry', 'Jewelry'),
+    ('book', 'Books & Magazines'),
+    ('coin', 'Coins'),
+    ('fashion', 'Fashion'),
+    ('decoration', 'Decoration'),
+    ('machine', 'Machines'),
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     image = models.ImageField(upload_to='main/static/uploads/', default="")
-    bio = models.CharField(max_length=250)
+    bio = models.TextField(max_length=250)
     rating = models.IntegerField()
 
     def __str__(self):
         return self.user.username
 
-
+class Auction(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    description = models.TextField(max_length=250)
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    end_time = models.DateTimeField()
+    category = models.CharField(max_length=12, choices=CATEGORY, default=CATEGORY[0][0])
