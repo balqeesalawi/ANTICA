f379ed04e1045e6fa872cc1d752ad3fb7551ccc5
Balqeesalawi <balqeesalawi195@gmail.com>
2025-11-11 12:09:40 +0300

    add sign up

--- ANTICA/main/views.py
+++ ANTICA/main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 
 
@@ -11,6 +13,20 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid Sign Up, Try Again'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 # @login_required
 # def profile(request):
 #     return render(request, 'users/profile.html')
