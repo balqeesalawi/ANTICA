09d43b7ac3b11798923eb68085a876fef4ef2088
Balqeesalawi <balqeesalawi195@gmail.com>
2025-11-11 19:38:36 +0300

    add delete and update views for Auction(complete all auction views)

--- ANTICA/main/urls.py
+++ ANTICA/main/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('auctions/', views.auctions_index, name='index'),
     path('auctions/<int:pk>', views.AuctionDetail.as_view(), name='auctions_detail'),
     path('auctions/create/', views.AuctionCreate.as_view(), name='auctions_create'),
+    path('auctions/<int:pk>/update/', views.AuctionUpdate.as_view(), name='auctions_update'),
+    path('auctions/<int:pk>/', views.AuctionDelete.as_view(), name='auctions_delete'),
 
     path('accounts/signup/', views.signup, name='signup')
 ]
--- ANTICA/main/views.py
+++ ANTICA/main/views.py
@@ -50,3 +50,12 @@ class AuctionCreate(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class AuctionUpdate(LoginRequiredMixin, UpdateView):
+    model = Auction
+    fields = ['current_price']
+
+class AuctionDelete(LoginRequiredMixin, DeleteView):
+    model = Auction
+    success_url = '/auctions/'
