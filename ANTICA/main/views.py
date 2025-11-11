c7603701ed30e7eff3456059dfd1c79b77f130b7
Balqeesalawi <balqeesalawi195@gmail.com>
2025-11-11 11:04:00 +0300

    add home and about

--- ANTICA/main/views.py
+++ ANTICA/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+# @login_required
+# def profile(request):
+#     return render(request, 'users/profile.html')
